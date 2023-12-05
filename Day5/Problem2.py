06330e4da35e13194ba299e358d8edb278728687
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2023-12-05 02:04:31 -0800

    part 2 attempt; feels very close but off by a tiny bit on the final stretch

--- Day5/Problem2.py
+++ Day5/Problem2.py
@@ -1,6 +1,7 @@
 import re
+import sys
 
-with open('input.txt') as f:
+with open('example.txt') as f:
     lines = [line.rstrip() for line in f]
 
 maps = [[0 for i in range(1)] for j in range(7)]
@@ -12,19 +13,7 @@ for i in range(1, len(lines)):
         partitions.append(i) # finds all instances of :
 
 # seeds = lines[1].split() # seeds are different, they don't go in the 2d array
-seedInput = lines[1].split()
-for i in range(len(seedInput)):
-    seedInput[i] = int(seedInput[i])
-seeds = []
-
-j = 0
-while j < len(seedInput) - 1:
-    print('a')
-    # seedInput[i], seedInput[i+1] = int(seedInput[i]), int(seedInput[i+1])
-    for j in range(seedInput[j+1]):
-        seeds.append(seedInput[i] + j)
-    i += 2
-
+seeds = lines[1].split()
 
 for i in range(len(partitions) - 1):
     maps[i] = lines[partitions[i] + 1:partitions[i + 1] - 1]
@@ -42,11 +31,9 @@ for i in range(len(maps)):
         dest, src, rng = int(dest), int(src), int(rng) # they're originally strings
         values[i].append([dest, src, rng]) # appends a triple to be used later
 
-currentValues = seeds
-for i in range(len(currentValues)):
-    currentValues[i] = int(currentValues[i]) # they are strings before this
-
-
+cv = seeds # stands for currentValue, name was just too long
+for i in range(len(cv)):
+    cv[i] = int(cv[i]) # they are strings before this
 
 for i in range(len(values)):
     del values[i][0:3]
@@ -54,20 +41,56 @@ for i in range(len(values)):
     # and due to how I initialized values
     # there's an extra three 0s at the beginning of each element, this removes it
 
+# treat currentValues array as a set of ranges
+# each pair is starting value, range
+
+print(cv)
+
+# dest + initial - source, min(c[k+1], range)
+# after first iteration it should be: [(50+79-52) min(14, 48) 50+(55-52) min(13, 48)]
+# [77, 14, 53, 13]
+
+"""
+79 14 55 13
+
+50 98 2
+52 50 48
+-----------------
+
+77, 14, 53, 13
+0 15 37
+37 52 2
+39 0 15
+
+77, 14, 38, 2
+"""
 for i in range(len(values)): # iterate over each map
-    changed = [False for z in range(len(currentValues))] # see conditional below
+    changed = [False for z in range(len(cv))]
+    # if k, k+1 are the indices of a pair, just set both to True if that range was changed
     for j in range(len(values[i])): # iterate over each line within the map
         dest, src, rng = values[i][j][0], values[i][j][1], values[i][j][2] # for my own benefit
-        for k in range(len(currentValues)): # iterate over each currently tracked value
-            # print(currentValues[k])
-            if currentValues[k] < src + rng and currentValues[k] >= src:
+        for k in range(0, int(len(cv)) - 1, 2):
+            # currentValues[k] will be each starting value of a range
+            if cv[k] + cv[k + 1] >= src and src + rng >= cv[k]:
                 if not changed[k]: # avoids double changing within a category
-                    changed[k] = True
+                    # print(f"changed on dest, src, rng {dest}, {src}, {rng} with initial & range {cv[k]}, {cv[k+1]}")
+                    changed[k], changed[k+1] = True, True
                     # print(f"found source {currentValues[k]}, with source and range {src}, {rng} on map number {i+1}, and changed it to {dest + currentValues[k] - src}")
-                    currentValues[k] = dest + currentValues[k] - src
+                    cv[k] = dest + cv[k] - src
+                    tmp2 = min(cv[k+1], rng)
+                    # cv[k] = cv[k] + src - dest
+                    # cv[k] = max(src, cv[k]) - abs(src - dest)
+                    cv[k + 1] = tmp2
             else:
-                currentValues[k] = currentValues[k] # honestly not needed, just for clarity
+                pass
+                # print(f"not changed on dest, src, rng {dest}, {src}, {rng}") # honestly not needed, just for clarity
+# 82, 84, 84, 84, 77, 45, 46, 46
 
-    print(currentValues)
+    print(cv)
 
-print(min(currentValues))
+minStart = cv[0]
+for k in range(0, int(len(cv)) - 1, 2):
+    if cv[k] < minStart:
+        minStart = cv[k]
+
+print(minStart)
