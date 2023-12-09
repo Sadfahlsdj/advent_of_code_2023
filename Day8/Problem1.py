83de2c4fec39b6947360c360db40ce087f4f3d45
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2023-12-08 17:04:45 -0800

    p2 attempt, ended up borrowing p1 code to use as a function and running said function for each starting input
    p1 and p2 have been commented
    may go back later and try to figure out why my original p2 attempt was broken (worked for 2 inputs, broken on the other 4)

--- Day8/Problem1.py
+++ Day8/Problem1.py
@@ -3,58 +3,43 @@ from math import gcd
 with open('input.txt') as f:
     lines = [line.rstrip() for line in f]
 
-# everything that ends in Z has only one path
-# everything that ends in A is unreachable normally
-
 directions = lines[0] # 281 length
 locations = []
-
-locationDict = {}
 
 for l in lines[2:]:
     location = l[0:3]
     left = l[7:10]
     right = l[12:15]
+    locations.append([location, left, right]) # each element is an array of (current, left, right)
 
-    locations.append([location, left, right])
-    locationDict.update({location: [left, right]})
-
-# print(locationDict) # key is location, values is an array with [left location, right location]
-
-# print(locations)
-locationToStart = -1
 for l in locations:
     if l[0] == 'AAA':
         locationToStart = locations.index(l)
-current = locations[locationToStart]
+current = locations[locationToStart] # makes sure i start in the right spot(IMPORTANT!!)
 counter = 0
 
 found = False
 while not found:
     for i in range(len(directions)):
-        print(f"current location is {current}, direction is {directions[i]},", end=" ")
-        counter += 1
-        loc = current[0]
+        print(f"current location is {current}, direction is {directions[i]},", end=" ") # diagnostic
+        counter += 1 # output counting
+        loc = current[0] # will be the current location
         # print(f"current location is {current}, current direction is {directions[i]}")
         if directions[i] == 'L':
             indexToFind = 1
-        elif directions[i] == 'R':
+        elif directions[i] == 'R': # decides which place to go to based on the current direction
             indexToFind = 2
         print(f"going to {current[indexToFind]}")
         for l in locations:
-            if l[0] == current[indexToFind]:
+            if l[0] == current[indexToFind]: # searches for the target location
                 print(f"found location {l} that has location {current[indexToFind]} with count {counter}")
                 current = l
-                break
-        if current[0] == 'ZZZ':
+                break # saves a little bit of speed
+        if current[0][2] == 'Z': # same as current[0]=ZZZ, this was done to help with p2
             found = True
             break
 
-
 print(counter)
-
-
-directionsCurrent = directions
 
 
 
--- Day8/Problem2.py
+++ Day8/Problem2.py
@@ -5,7 +5,33 @@ with open('input.txt') as f:
     lines = [line.rstrip() for line in f]
 
 def lcm(numbers):
-    return reduce(lambda x, y: x * y // gcd(x, y), numbers, 1)
+    return reduce(lambda x, y: x * y // gcd(x, y), numbers, 1) # for use later
+
+def findZ(currentOriginal, current, counter, directions):
+    # pretty much identical to p1 code, probably missing some comments
+    found = False
+    while not found:
+        for i in range(len(directions)):
+            # print(f"current location is {current}, direction is {directions[i]},", end=" ")
+            counter += 1
+            loc = current[0]
+            # print(f"current location is {current}, current direction is {directions[i]}")
+            if directions[i] == 'L':
+                indexToFind = 1
+            elif directions[i] == 'R':
+                indexToFind = 2
+            # print(f"going to {current[indexToFind]}")
+            for l in locations:
+                if l[0] == current[indexToFind]:
+                    # print(f"found location {l} that has location {current[indexToFind]} with count {counter}")
+                    current = l
+                    break
+            if current[0][2] == 'Z':
+                found = True
+                print(f"original is {currentOriginal} and endpoint is {current} with count {counter}")
+                break
+    return counter
+
 
 # everything that ends in Z has only one path
 # everything that ends in A is unreachable normally
@@ -13,65 +39,45 @@ def lcm(numbers):
 directions = lines[0] # 281 length
 locations = []
 
-locationDict = {}
-
 for l in lines[2:]:
     location = l[0:3]
     left = l[7:10]
     right = l[12:15]
-
     locations.append([location, left, right])
-    locationDict.update({location: [left, right]})
 
 # print(locationDict) # key is location, values is an array with [left location, right location]
 
 current = []
-ends = []
-# print(locations)
-locationToStart = -1
+
 for l in locations:
     if l[0][2] == 'A':
-        current.append(l)
+        current.append(l) # starts as an array of all "locations" that have current end in a
 
-counter = [0] * len(current)
+currentOriginal = current.copy() # used for diagnostics
+
+counter = [0] * len(current) # array of counts, one for each starting location
+ends = [['AAA', 'AAA', 'AAA']] * len(current) # used for diagnostics
 print(current)
-found = [False] * len(current)
-print(found)
-while not all(f for f in found):
-    for i in range(len(directions)):
-        for j in range(len(current)):
-            if not found[j]:
-                # print(f"current location is {current}, direction is {directions[i]},", end=" ")
-                counter[j] += 1
-                loc = current[j][0]
-                # print(f"current location is {current}, current direction is {directions[i]}")
-                if directions[i] == 'L':
-                    indexToFind = 1
-                elif directions[i] == 'R':
-                    indexToFind = 2
-                # print(f"going to {current[j][indexToFind]}")
-                for l in locations:
-                    if l[0] == current[j][indexToFind]:
-                        # print(f"found location {l} that has location {current[j][indexToFind]} with count {counter}")
-                        current[j] = l
-                        break
-                if current[j][0][2] == 'Z':
-                    found[j] = True
-                    ends.append(current[j][0])
-                    break
+found = [False] * len(current) # one for each starting location
+# print(found)
 
 
+for i in range(len(counter)):
+    counter[i] = findZ(currentOriginal[i], current[i], counter[i], directions)
+    # updates counter array with each individual count
 print(counter)
-print(lcm(counter))
-
 print(ends)
+
+print(lcm(counter)) # lcm of all individual counts will tell us when every z is concurrently found
 
 """
 wrong answer:
 34324276586199360428040
+right answer:
+15995167053923
 """
 
-directionsCurrent = directions
+
 
 
 
